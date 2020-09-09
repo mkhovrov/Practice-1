3b7452ff8e161acf7b163abe5ed5f2f418086aed
khovre <mkhovrov01@gmail.com>
2020-09-09 19:34:19 +0300

    Initial commit

---
+++ src/HarmonicSeries.java
@@ -0,0 +1,12 @@
+public class HarmonicSeries
+{
+    public static void main(String[]args)
+    {
+        for (double i = 1; i <= 10; i++)
+        {
+            double b = 1 / i;
+            System.out.println(b);
+        }
+    }
+}
+
